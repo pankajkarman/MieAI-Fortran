38af95e1c5c48ce6357ddd5c379dc4fc23f4a657
Pankaj Kumar <pankaj.kmr1990@gmail.com>
2023-09-05 16:09:33 +0200

    added read_min_max subroutine and test

---
+++ utils/test_scaler.F90
@@ -0,0 +1,19 @@
+program test_scaler
+  use scaler, only: read_min_max_data
+  implicit none 
+    
+    integer :: i
+    integer, parameter :: nrows=13
+    real(8), dimension(nrows) :: min_vals, max_vals
+    character(20), dimension(nrows) :: vname
+    character(len=256), dimension(:), allocatable :: args
+    
+    allocate(args(1))
+    
+    call get_command_argument(1,args(1))
+    call read_min_max_data(args(1), nrows, vname, min_vals, max_vals)   
+    do i=1,13
+        write(*, *) vname(i), min_vals(i), max_vals(i)
+    end do
+
+end program test_scaler
